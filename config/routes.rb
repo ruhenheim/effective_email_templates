bfdccb84ff1849bcb3306fa7c49695b38bf71809
ruhenheim <asd.uchiyama@gmail.com>
2017-02-13 14:53:11 +0900

    frexible routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
-Rails.application.routes.draw do
-  mount EffectiveEmailTemplates::Engine => '/', :as => 'effective_email_templates'
-end
-
 EffectiveEmailTemplates::Engine.routes.draw do
   if defined?(EffectiveDatatables)
-    namespace :admin do
-      resources :email_templates, only: [:index, :edit, :update]
-    end
+    resources :email_templates, only: [:index, :edit, :update]
   end
 end
